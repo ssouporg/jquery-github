a5cc5d717bf4a7f101f42c1a001ecb718ddc104c
Alessandro Bellucci <belluster@gmail.com>
2012-07-13 13:56:03 +0300

    Update gh-pages

--- jquery.github-1.0.js
+++ jquery.github-1.0.js
@@ -6,6 +6,10 @@
 	init: function( options ) {
 	},
 
+	function isAuthorized() {
+		return getUrlVars().access_token != undefined;
+	}
+
 	/**
 	 * Returns the github raw URL of a given resource.
 	 */
@@ -327,6 +331,22 @@
 		}
 	}
 	return path;
+  }
+
+  /**
+   * Read a page's GET URL variables and return them as an associative array.
+   */
+  function getUrlVars()
+  {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
   }
 
   function get( url ) {
